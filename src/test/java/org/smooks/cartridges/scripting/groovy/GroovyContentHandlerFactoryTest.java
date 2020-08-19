1c24d5fe5f1c69c8ec092a86a9eae1810fbbf921
claude.mamo <claude.mamo@gmail.com>
2020-08-19 15:27:32 +0200

    fix: solve compilation errors caused by API changes in Smooks 2

--- src/test/java/org/smooks/cartridges/scripting/groovy/GroovyContentHandlerFactoryTest.java
+++ src/test/java/org/smooks/cartridges/scripting/groovy/GroovyContentHandlerFactoryTest.java
@@ -43,8 +43,11 @@
 package org.smooks.cartridges.scripting.groovy;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.smooks.cdr.SmooksResourceConfiguration;
+import org.smooks.cdr.injector.Scope;
+import org.smooks.cdr.lifecycle.phase.PostConstructLifecyclePhase;
+import org.smooks.cdr.registry.lookup.LifecycleManagerLookup;
+import org.smooks.container.MockApplicationContext;
 import org.smooks.delivery.dom.DOMElementVisitor;
 import org.smooks.io.StreamUtils;
 import org.smooks.xml.XmlUtil;
@@ -54,35 +57,39 @@ import org.xml.sax.SAXException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * @author tfennelly
  */
 public class GroovyContentHandlerFactoryTest {
 
-        @Test
+	@Test
 	public void test_goodscript_by_URI() throws InstantiationException, IllegalArgumentException, IOException, SAXException {
 		test_goodscript_by_URI("classpath:/org/smooks/cartridges/scripting/groovy/MyGroovyScript.groovy");
 		test_goodscript_by_URI("/org/smooks/cartridges/scripting/groovy/MyGroovyScript.groovy");
 	}
 
-        @Test
+	@Test
 	public void test_goodscript_by_Inlining() throws InstantiationException, IllegalArgumentException, IOException, SAXException {
 		String script = new String(StreamUtils.readStream(getClass().getResourceAsStream("MyGroovyScript.groovy")));
 		SmooksResourceConfiguration config = new SmooksResourceConfiguration("x", script);
-		
+
 		test_goodscript(config);
 	}
-	
+
 	private void test_goodscript_by_URI(String path) throws InstantiationException, IllegalArgumentException, IOException, SAXException {
 		test_goodscript(new SmooksResourceConfiguration("x", path));
 	}
-	
+
 	private void test_goodscript(SmooksResourceConfiguration config) throws InstantiationException, IllegalArgumentException, IOException, SAXException {
 		GroovyContentHandlerFactory creator = new GroovyContentHandlerFactory();
+		MockApplicationContext mockApplicationContext = new MockApplicationContext();
+		mockApplicationContext.getRegistry().lookup(new LifecycleManagerLookup()).applyPhase(creator, new PostConstructLifecyclePhase(new Scope(mockApplicationContext.getRegistry())));
 		
 		config.setParameter("new-name", "yyy");
 		DOMElementVisitor resource = (DOMElementVisitor) creator.create(config);
-		
+
 		Document doc = XmlUtil.parseStream(new ByteArrayInputStream("<xxx/>".getBytes()), XmlUtil.VALIDATION_TYPE.NONE, false);
 		assertEquals("xxx", doc.getDocumentElement().getTagName());
 		resource.visitAfter(doc.getDocumentElement(), null);
